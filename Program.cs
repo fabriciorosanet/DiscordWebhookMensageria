3be7c769c6d50549ae9f30c36a40e67bc96ebbba
Fabrício Rosa <161895481+fabriciorosanet@users.noreply.github.com>
2024-09-19 19:45:56 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -38,22 +38,14 @@ public class DiscordWebhookSender
     // Lista de webhooks
     public static readonly Dictionary<string, string> Webhooks = new Dictionary<string, string>
     {
-        { "CANAL - EVENTOS", "https://discord.com/api/webhooks/1252710100867481616/HPw1Vu8uQF1ZNgrU9N8kkhdXc4FA90GLW_43rFbvJJ8-7lcBXW9gYvITTfw2Je_PuCt1" },
-        { "1FRNT - AVISO", "https://discord.com/api/webhooks/1270836670538846239/JdZCekfMxgxaO6UUZdvQIt0WWylVzwrwLrPxg5Lam5unvqyIUuqgj0pJqA2tai8jPesh" },
-        { "1FRNT - BATE-PAPO", "https://discord.com/api/webhooks/1270838654050242611/abZ5a0yfNYZetkeXsT254kYePvHpO3XTjVldByQJVnj5fDK8MuGJN5WFTmS0QjWGOKBT" },
-        { "1DPTM - AVISO", "https://discord.com/api/webhooks/1270839020749979678/6OCasUnpJDWNbJS3DO0wECSFKk8acjAuy8GFcwE2W9E8ZRh8BPzYuFuEgwm1X3zfaXc8" },
-        { "1DPTM - BATE-PAPO", "https://discord.com/api/webhooks/1270839485671669865/GtOxZS7VgkSlAXhua_A4Z8kAU2cwGYMOMVtm6Qz4ugEB4itvyB3E3bDA0_1QRX2LAS9g" },
         // Adicione os outros webhooks aqui da mesma maneira
     };
 
     public static async Task Main(string[] args)
     {
         // Exemplo de envio de mensagem
-        var webhookUrl = Webhooks["CANAL - EVENTOS"];
-        var message = "@here\n" +
-                    " **Alerta: Seu evento irá começar! 🔔 **\n\n" +
-                    "Em 30 minutos iniciaremos nosso encontro de hoje. Clique na seção 'Eventos' no canto superior esquerdo do Discord e acesse sua atividade.\n\n" +
-                    "Esperamos você!\n\n";
+        var webhookUrl = Webhooks[ ]; //liste o webhook
+        var message = ; // passe sua string de mensagem aqui!
         await SendToDiscord(webhookUrl, message);
     }
 }
